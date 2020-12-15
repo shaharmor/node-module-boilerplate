870aa2659e1d1d35c1b3d463cd558a48ad625dc1
Shahar Mor <shaharmor1@gmail.com>
2020-12-15 12:30:34 +0200

    use new @shaharmor/eslint-config format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
   extends: ['@shaharmor/eslint-config'],
   env: {
     node: true,
     jest: true,
   },
+  parserOptions: {
+    tsconfigRootDir: __dirname
+  }
 };
